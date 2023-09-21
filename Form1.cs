4baa4fdd8f5e3b9207f1962a4c773bb436bc4648
Lysenko Vladyslav <vlad.lysenko1999@gmail.com>
2023-09-21 18:53:23 +0300

    Lab ex 1 was Done

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace WinFormApp_2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Hello Lab 1");
+        }
     }
 }
